be875e2431bc1ae33f57fcfa9330369f3057bc29
DevHackNSlash <100962767+DevHackNSlash@users.noreply.github.com>
2023-04-09 19:47:49 -0400

    Add files via upload

---
+++ CollegeRegistration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWS120Project
+{
+    internal class CollegeRegistration
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
